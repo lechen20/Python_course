185ac215689db266d747299c21aab69ae5757685
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-10-09 20:21:12 +0800

    Update question3.py

--- python_course/Experiments/code3/question3.py
+++ python_course/Experiments/code3/question3.py
@@ -1,1 +1,16 @@
 # 第三题：括号匹配（Valid Braces）
+def valid_braces(string):
+    list=[]
+    for n in string:
+        if n=="(" or n=="[" or n=="{":
+            list.append(n)
+        elif n==')' and (not list or list[-1]!='(' ):  #not list不是空列表 or 最后一个元素不是'('
+            return False
+        elif n==']' and (not list or list[-1]!='[' ):
+            return False
+        elif n=='}' and (not list or list[-1]!='{'):
+            return False
+        else:
+            list.pop()                                 #删除
+    return not list                                    #非空为True
+            
