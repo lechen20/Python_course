c3d19dcda80d94597b7369b556c9bae4ba6d4d7d
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-06 12:12:17 +0800

    Update question3.py

--- python_course/Experiments/code5/question3.py
+++ python_course/Experiments/code5/question3.py
@@ -1,1 +1,11 @@
 
+第三题： 检测Pangram    
+
+def is_pangram(s):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    s = s.lower()
+    s = ''.join(filter(str.isalpha, s))
+    for letter in alphabet:
+        if letter not in s:
+            return False
+    return True
