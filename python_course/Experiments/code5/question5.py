0541871acc1f630d7c41536348f73211cc3fd02a
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-06 12:13:03 +0800

    Update question5.py

--- python_course/Experiments/code5/question5.py
+++ python_course/Experiments/code5/question5.py
@@ -1,1 +1,11 @@
+# 第五题： 疯狂的彩色三角形    
+def triangle(row):
+    reduce=[3**i+1 for i in range(10) if 3**i<=100000][::-1]
+    
+    COLOR = {'GG':'G', 'BB':'B', 'RR':'R', 'BR':'G', 
+            'BG':'R', 'GB':'R', 'GR':'B', 'RG':'B', 'RB':'G'}
 
+    for length in reduce:
+        while len(row)>=length:
+            row=[ COLOR[row[i] + row[i+length-1]] for i in range(len(row)-length+1)]
+    return row[0]
