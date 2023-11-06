a60f30ac36ab7eb861d42cb45db99fa3e1a8620a
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-06 12:11:53 +0800

    Update question2.py

--- python_course/Experiments/code5/question2.py
+++ python_course/Experiments/code5/question2.py
@@ -1,1 +1,17 @@
-
+# 第二题： 发现离群的数(Find The Parity Outlier)    
+def find_outlier(integers):
+    even_num=0
+    odd_num=0
+    even=None
+    odd=None
+    for i in integers:
+        if(i%2==0):
+            odd_num+=1
+            odd=i
+        else:
+            even_num+=1
+            even=i
+    if(odd_num>even_num):
+        return even
+    else:
+        return odd
