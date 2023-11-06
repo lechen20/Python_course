41a70876b9d5fc4184533495a75f9a43f04ba912
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-06 12:12:41 +0800

    Update question4.py

--- python_course/Experiments/code5/question4.py
+++ python_course/Experiments/code5/question4.py
@@ -1,1 +1,23 @@
 
+# 第四题： 数独解决方案验证    
+
+def validate_sudoku(board):
+    elements = set(range(1, 10))
+
+    for b in board:
+        if set(b) != elements: 
+            return False
+    for b in zip(*board):  
+        if set(b) != elements: 
+            return False
+
+    for i in range(3, 10, 3):
+        for j in range(3, 10, 3):
+            elements_set = set()
+            for w in range(j-3, j):
+                for q in range(i-3, i):
+                    elements_set.add(board[q][w])
+            if elements != elements_set:
+                return False
+            
+    return True
