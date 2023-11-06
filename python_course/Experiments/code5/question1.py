2d02bc0e3bde28ce33c3d325e6ccf1c8abed1d76
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-06 12:11:26 +0800

    Update question1.py

--- python_course/Experiments/code5/question1.py
+++ python_course/Experiments/code5/question1.py
@@ -1,1 +1,9 @@
-
+# 第一题：停止逆转我的单词   
+def spin_words(sentence):
+    words = sentence.split()  
+    words1 = []
+    for word in words:
+        if len(word) > 4: 
+            word = word[::-1] 
+        words1.append(word)
+    return ' '.join(words1) 
