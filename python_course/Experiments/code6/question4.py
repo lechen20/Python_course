592f06478f6a7de9675c16f3fb07d13bbff2a94b
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-13 08:37:08 +0800

    Update question4.py

--- python_course/Experiments/code6/question4.py
+++ python_course/Experiments/code6/question4.py
@@ -1,1 +1,12 @@
-
+# 第四题： 编码聚会7 
+def find_senior(lst):
+    max=0
+    lis=[]
+    for lsts in lst:
+        if lsts['age']>max:
+            lis.clear()
+            lis.append(lsts)
+            max=lsts['age']
+        elif lsts['age']==max:
+            lis.append(lsts)
+    return lis
