c0a4effd45427b02d37079483ed29fa794b7c569
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-13 08:35:46 +0800

    Update question1.py

--- python_course/Experiments/code6/question1.py
+++ python_course/Experiments/code6/question1.py
@@ -1,1 +1,7 @@
-
+# 第一题：编码聚会1
+def count_developers(lst):
+    count=0
+    for lsts in lst:
+        if lsts['continent']=='Europe' and lsts['language']=='JavaScript':
+            count+=1
+    return count
