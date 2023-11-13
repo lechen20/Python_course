9ebc782e4d1b2c1c47411d49b47abf5bba8c6a12
lechen20 <105703231+lechen20@users.noreply.github.com>
2023-11-13 08:36:46 +0800

    Update question3.py

--- python_course/Experiments/code6/question3.py
+++ python_course/Experiments/code6/question3.py
@@ -1,1 +1,17 @@
+# 第三题： 缩短数值的过滤器(Number Shortening Filter)
+
+def shorten_number(suffixes,base):
+    def my_filter(data):
+        try:
+            number = int(data)
+        except (TypeError, ValueError):
+            return str(data)
+        else:
+            i = 0
+            while number//base > 0 and i < len(suffixes)-1:
+                number //= base
+                i += 1
+            return str(number) + suffixes[i] 
+    
+    return my_filter
 
